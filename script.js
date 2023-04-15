b15b857a688942c2186fb16fc1786b16714cc047
CamiloPalaciosT <124638638+CamiloPalaciosT@users.noreply.github.com>
2023-04-15 02:04:47 -0500

    pagina de encriptador, sin web responsive

---
+++ script.js
@@ -0,0 +1,36 @@
+function encriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"ines");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function desencriptar() {
+    var texto = document.getElementById("inputTexto").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/ines/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+    document.getElementById("imgDer").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+}
+
+function cop() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+    alert("Su texto ha sido copiado con exito");
+}
